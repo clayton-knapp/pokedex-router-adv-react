b9168b59db9c17f95920cd2dd0ce0aeb8e7c8309
Clayton Knapp <csjknapp@gmail.com>
2022-05-09 10:33:56 -0700

    added routes in App and Router in index

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import {
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import List from './views/List';
+import Detail from './views/Detail';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Switch>
+
+      {/* Route for Detail */}
+      <Route path='/pokemon/:id'>
+        <Detail />
+      </Route>
+
+      {/* Route for List */}
+      <Route path='/pokemon'>
+        <List />
+      </Route>
+
+      {/* Route for Home -> Redirect to List for now */}
+      <Route path='/'>
+        <Redirect exact from="/" to="/pokemon" />
+      </Route>
+
+    </Switch>
+  );
 }
